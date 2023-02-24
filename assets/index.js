e11d3453fa2b3499f4214693fc16373392f031b5
Marvalyn <111514523+Marvalyn@users.noreply.github.com>
2023-02-24 22:57:07 +0000

    manager inquirer object added

--- assets/index.js
+++ assets/index.js
@@ -4,6 +4,7 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
+const inquirer = require("inquirer");
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
@@ -13,3 +14,27 @@ const render = require("./src/page-template.js");
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
+function addManager() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: "Manager Name",
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: "Manager ID number",
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: "Manager email address",
+        },
+        {
+            type: 'input',
+            name: 'officeNumber',
+            message: "Manager office number?",
+        }
+    ])
+}
