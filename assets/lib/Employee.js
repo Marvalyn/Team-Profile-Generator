9c341934f503027d88a31982608af33cb1133861
Marvalyn <111514523+Marvalyn@users.noreply.github.com>
2023-02-24 23:13:50 +0000

    constructor added for employee

--- assets/lib/Employee.js
+++ assets/lib/Employee.js
@@ -1,1 +1,31 @@
 // TODO: Write code to define and export the Employee class
+// setting constructor class for Employee
+
+class Employee {
+    //passing parameters for name, id, email and role
+    constructor(name, id, email, role) {
+        this.name = name;
+        this.id = id;
+        this.email = email;
+        this.role = role;
+    }
+
+    //getName() required
+    getName() {
+        return this.name
+    }
+    //getID() required
+    getId() {
+        return this.id
+    }
+    //getEmail() required
+    getEmail() {
+        return this.email
+    }
+    //getRole() required -- returns 'Employee'
+    getRole() {
+        return this.role
+    }
+}
+
+module.exports = Employee;
