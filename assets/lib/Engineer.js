037777803402beafcd8a8132b99784d3b42b1f56
Marvalyn <111514523+Marvalyn@users.noreply.github.com>
2023-02-24 23:47:46 +0000

    constructor classes added, tests not passing

--- assets/lib/Engineer.js
+++ assets/lib/Engineer.js
@@ -1,1 +1,19 @@
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+const Employee = require("../lib/Employee");
+
+class Engineer extends Employee {
+    constructor(name, id, email, role, github) {
+        super(name, id, email, role);
+
+        this.github = github;
+    }
+    getGithub() {
+        return this.github
+    }
+
+    getRole() {
+        return "Engineer"
+    }
+}
+
+module.exports = Engineer;
